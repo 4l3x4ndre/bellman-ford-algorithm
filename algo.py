f383a8513e48cd2e9bfc91fe835e1d3ba5e0ec1a
4l3x4ndre <alexandre@amrani.fr>
2021-03-04 23:34:10 +0100

    updated names

--- algo.py
+++ algo.py
@@ -1,31 +1,31 @@
-from math import inf as infinity
-from graphe import GrapheMatPond
+from math import inf
+from graphe import GraphMatrixWeighted
 
-g = GrapheMatPond(6)
-g.ajouter_arc(0, 1, 1)
-g.ajouter_arc(1, 2, 4)
-g.ajouter_arc(2, 3, 1)
-g.ajouter_arc(3, 4, 20)
-g.ajouter_arc(1, 4, 200)
-g.ajouter_arc(0, 2, 50)
-g.ajouter_arc(1, 5, 100)
-g.ajouter_arc(5, 4, 1)
+g = GraphMatrixWeighted(6)
+g.add_edge(0, 1, 1)
+g.add_edge(1, 2, 4)
+g.add_edge(2, 3, 1)
+g.add_edge(3, 4, 20)
+g.add_edge(1, 4, 200)
+g.add_edge(0, 2, 50)
+g.add_edge(1, 5, 100)
+g.add_edge(5, 4, 1)
 
 
-def bf(g, s):
-    distances = [infinity] * len(g.sommets())
+def belmannford(g, s):
+    distances = [inf] * len(g.tops())
     distances[0] = 0
 
-    for k in range(1, len(g.sommets())):
-        sommet_courant = g.sommets()[k]
-        for voisin in g.voisins(sommet_courant):
-            distances[sommet_courant] = min(
-                distances[sommet_courant],
-                distances[voisin] + g.arc(voisin, sommet_courant)
+    for k in range(1, len(g.tops())):
+        top = g.tops()[k]
+        for neighbour in g.neighbours(top):
+            distances[top] = min(
+                distances[top],
+                distances[neighbour] + g.edge_weight(neighbour, top)
             )
 
     return distances
 
 
-d = bf(g, 0)
+d = belmannford(g, 0)
 print(d)
--- graphe.py
+++ graphe.py
@@ -1,25 +1,22 @@
-class GrapheMatPond:
-    """Un graphe représenté par une matrice d'adjacence,
-       où les sommets sont les entiers $0, 1, ..., n - 1"""
+class GraphMatrixWeighted:
 
     def __init__(self, n):
         self.adj = [[0] * n for _ in range(n)]
         self.n = n
 
-    def ajouter_arc(self, s1, s2, p):
+    def add_edge(self, s1, s2, p):
         self.adj[s1][s2] = p
         self.adj[s2][s1] = p
 
-    def arc(self, s1, s2):
+    def edge_weight(self, s1, s2):
         return self.adj[s1][s2]
 
-    def voisins(self, s):
+    def neighbours(self, s):
         l = []
         for i in range(len(self.adj[s])):
             if self.adj[s][i] != 0:
                 l.append(i)
-
         return l
 
-    def sommets(self):
+    def tops(self):
         return [x for x in range(self.n)]
